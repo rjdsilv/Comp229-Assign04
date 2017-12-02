09d700999d7dfd556693618fbdd52ff1a19fa1cb
Rodrigo Januário da Silva <rjdsilv@gmail.com>
2017-12-02 18:39:11 -0500

    Fixed the trace method.

--- Comp229-Assign04/Helpers/JsonReaderHelper.cs
+++ Comp229-Assign04/Helpers/JsonReaderHelper.cs
@@ -7,46 +7,42 @@ using System.IO;
 
 namespace Comp229_Assign04.Helpers
 {
+    /// <summary>
+    /// Helper class to deal with JSON Serialization and Deserialization.
+    /// </summary>
     public class JsonHelper
     {
         // The file location.
         private const string JSON_FILE_NAME = @"Data\Assign04.json";
 
-        // The application logger.
-        private static TraceSource TRACE = new TraceSource("ApplicationLogger");
-
+        /// <summary>
+        /// Deserializes the content from the file contained in JSON_FILE_NAME into a list of Mini objects.
+        /// </summary>
+        /// <param name="basePath">The JSON file base path.</param>
+        /// <returns>The list of Minis if everything is ok. An empty list otherwise.</returns>
         public static List<Mini> Deserialize(string basePath)
         {
             // Build the file name.
-            var filePath = basePath;
-
-            if (filePath.EndsWith("\\"))
-            {
-                filePath += JSON_FILE_NAME;
-            }
-            else
-            {
-                filePath += "\\" + JSON_FILE_NAME;
-            }
+            var filePath = basePath.EndsWith("\\") ? basePath + JSON_FILE_NAME : basePath + "\\" + JSON_FILE_NAME;
 
             if (File.Exists(filePath))
             {
                 try
                 {
                     // Reads the file and deserialize it into a List of Mini objects.
-                    var jsonStr = File.ReadAllText(filePath);
-                    return JsonConvert.DeserializeObject<List<Mini>>(jsonStr);
+                    return JsonConvert.DeserializeObject<List<Mini>>(File.ReadAllText(filePath));
                 }
                 catch(Exception ex)
                 {
                     // If any error occurs, just return an empty list.
-                    TRACE.TraceEvent(TraceEventType.Error, 1001, string.Format("The file {0} cannot be used or could not be found!!!", JSON_FILE_NAME));
+                    Trace.TraceError(string.Format("The file {0} cannot be used or could not be found!!! - Exception message {1)", JSON_FILE_NAME, ex.Message));
                     return new List<Mini>();
                 }
             }
 
             // If the file does not exist, just return an empty list.
-            TRACE.TraceEvent(TraceEventType.Error, 1002, string.Format("The file {0} could not be found!!!", JSON_FILE_NAME));
+            Trace.TraceError(string.Format("The file {0} could not be found!!!", JSON_FILE_NAME));
+            Trace.Flush();
             return new List<Mini>();
         }
     }
